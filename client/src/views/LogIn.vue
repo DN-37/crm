6e06062574d440280ce88d2e060883ffec3c8869
Danila Nefedov <dn375@mail.ru>
2023-10-25 22:12:23 +0400

    created component Navbar, views LogIn and SignUp

---
+++ client/src/views/LogIn.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title">Авторизация</h1>
+
+                <form>
+                    <div class="field">
+                        <label>Электронная почта</label>
+                        <div class="control">
+                            <input type="email" name="email" class="input">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Пароль</label>
+                        <div class="control">
+                            <input type="password" name="password" class="input">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-success">Отправить</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LogIn'
+}
+</script>
