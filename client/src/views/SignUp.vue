14299cd8caa05f467c02b6742c2b8511edf74cd5
Danila Nefedov <dn375@mail.ru>
2023-10-30 21:26:44 +0400

    made SignUp

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -4,26 +4,30 @@
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Регистрация</h1>
 
-                <form>
+                <form @submit.prevent="submitForm">
                     <div class="field">
                         <label>Электронная почта</label>
                         <div class="control">
-                            <input type="email" name="email" class="input">
+                            <input type="email" name="email" class="input" v-model="username">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Пароль</label>
                         <div class="control">
-                            <input type="password" name="password1" class="input">
+                            <input type="password" name="password" class="input" v-model="password">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Повторите пароль</label>
                         <div class="control">
-                            <input type="password" name="password2" class="input">
+                            <input type="password" name="password_repeat" class="input" v-model="password_repeat">
                         </div>
+                    </div>
+
+                    <div class="notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
@@ -38,7 +42,59 @@
 </template>
 
 <script>
+import axios from 'axios'
+import { toast } from 'bulma-toast'
 export default {
-    name: 'SignUp'
+    name: 'SignUp',
+    data() {
+        return {
+            username: '',
+            password: '',
+            password_repeat: '',
+            errors: []
+        }
+    },
+    methods: {
+        submitForm() {
+            this.errors = []
+            if (this.username === '') {
+                this.errors.push('Имя пользователя отсутствует')
+            }
+            if (this.password.length < 8) {
+                this.errors.push('Пароль слишком короткий')
+            }
+            if (this.password !== this.password_repeat) {
+                this.errors.push('Пароли не совпадают')
+            }
+            if (!this.errors.length) {
+                const formData = {
+                    username: this.username,
+                    password: this.password
+                }
+                axios
+                    .post('/api/v1/users/', formData)
+                    .then(response => {
+                        toast({
+                            message: 'Аккаунт создан, пожалуйста, войдите',
+                            type: 'is-success',
+                            dismissible: true,
+                            pauseOnHover: true,
+                            duration: 2000,
+                            position: 'bottom-right',
+                        })
+                        this.$router.push('/log-in')
+                    })
+                    .catch(error => {
+                        if (error.response) {
+                            for (const property in error.response.data) {
+                                this.errors.push(`${property}: ${error.response.data[property]}`)
+                            }
+                        } else if (error.message) {
+                            this.errors.push('Что-то пошло не так. Пожалуйста, попробуйте еще раз!')
+                        }
+                    })
+            }
+        }
+    }
 }
 </script>
--- server/server/settings.py
+++ server/server/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
 
 # Application definition
 
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
